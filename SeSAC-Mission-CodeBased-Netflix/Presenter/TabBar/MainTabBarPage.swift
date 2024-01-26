8cce29e6eb17afdbb076f1271da0f63c371c37ba
Taeyoung Won <hexter1994@gmail.com>
2024-01-26 16:45:56 +0900

    [Add] 탭바 페이지 열거형 구현

---
+++ SeSAC-Mission-CodeBased-Netflix/Presenter/TabBar/MainTabBarPage.swift
@@ -0,0 +1,62 @@
+//
+//  MainTabBarPage.swift
+//  SeSAC-Mission-CodeBased-Netflix
+//
+//  Created by 원태영 on 1/26/24.
+//
+
+import UIKit
+
+enum MainTabBarPage: Int, CaseIterable {
+  
+  case home = 0
+  case search
+  case save
+  
+  var index: Int {
+    self.rawValue
+  }
+  
+  var title: String {
+    switch self {
+      case .home:
+        return "홈"
+        
+      case .search:
+        return "NEW & HOT"
+        
+      case .save:
+        return "저장한 콘텐츠 목록"
+    }
+  }
+  
+  var icon: UIImage? {
+    switch self {
+      case .home:
+        return UIImage(systemName: "house")
+        
+      case .search:
+        return UIImage(systemName: "play.rectangle.on.rectangle")
+        
+      case .save:
+        return UIImage(systemName: "arrow.down.circle")
+    }
+  }
+  
+  var selectedIcon: UIImage? {
+    switch self {
+      case .home:
+        return UIImage(systemName: "house.fill")
+        
+      case .search:
+        return UIImage(systemName: "play.rectangle.on.rectangle.fill")
+        
+      case .save:
+        return UIImage(systemName: "arrow.down.circle.fill")
+    }
+  }
+  
+  var tabBarItem: UITabBarItem {
+    return UITabBarItem(title: title, image: icon, selectedImage: selectedIcon)
+  }
+}
