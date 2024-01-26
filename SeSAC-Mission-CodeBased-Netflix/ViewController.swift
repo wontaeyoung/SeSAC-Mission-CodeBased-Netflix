b3c13ba8a124cceffaee2ff96ab7405d10992d45
Taeyoung Won <hexter1994@gmail.com>
2024-01-26 15:28:54 +0900

    Initial Commit

---
+++ SeSAC-Mission-CodeBased-Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-Mission-CodeBased-Netflix
+//
+//  Created by 원태영 on 1/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
